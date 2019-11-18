0597c6ecd613e68dd802d9ea1a193bb152ccf8ff
LAP01277 <topol.phukhanh@gmail.com>
2019-11-18 12:05:56 +0700

    add

---
+++ _posts/2019-11-18-150 successful machine learning models: 6 lessons learned at Booking.com.py
@@ -0,0 +1,29 @@
+---
+layout: post
+title: 150 successful machine learning models: 6 lessons learned at Booking.com
+categories: [Data Science]
+tags: [Data Science]
+description: 150 successful machine learning models: 6 lessons learned at Booking.com
+---
+
+# 150 successful machine learning models: 6 lessons learned at Booking.com
+
+150 successful machine learning models: 6 lessons learned at Booking.com Bernadi et al., KDD’19
+
+Here’s a paper that will reward careful study for many organisations. We’ve previously looked at the deep penetration of machine learning models in the product stacks of leading companies, and also some of the pre-requisites for being successful with it. Today’s paper choice is a wonderful summary of lessons learned integrating around 150 successful customer facing applications of machine learning at Booking.com. Oddly enough given the paper title, the six lessons are never explicitly listed or enumerated in the body of the paper, but they can be inferred from the division into sections. My interpretation of them is as follows:
+
+- Projects introducing machine learned models deliver strong business value
+- Model performance is not the same as business performance
+- Be clear about the problem you’re trying to solve
+- Prediction serving latency matters
+- Get early feedback on model quality
+- Test the business impact of your models using randomised controlled trials (follows from #2)
+
+There are way more than 6 good pieces of advice contained within the paper though!
+
+We found that driving true business impact is amazingly hard, plus it is difficult to isolate and understand the connection between efforts on modeling and the observed impact… Our main conclusion is that an iterative, hypothesis driven process, integrated with other disciplines was fundamental to build 150 successful products enabled by machine learning.
+
+In case you’re tempted to stop reading at this point, please don’t interpret that quote as saying that investing in machine learning isn’t worth it. On the contrary, developing an organisational capability to design, build, and deploy successful machine learned models in user-facing contexts is, in my opinion, as fundamental to an organisation’s competitiveness as all the other characteristics of high-performing organisations highlighted in the State of DevOps reports. (And by the way, wouldn’t it be wonderful to see data confirming or denying that hypothesis in future reports!).
+
+Reference:
+[150-successful-machine-learning-models-6-lessons-learned-at-booking.com](https://www.kdd.org/kdd2019/accepted-papers/view/150-successful-machine-learning-models-6-lessons-learned-at-booking.com)
